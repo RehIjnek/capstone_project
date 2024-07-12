fb37640089928e206cda28d0d3761e61f11fbf92
Kenji Her <kenjito7@gmail.com>
2024-07-12 13:29:06 -0400

    added changes to generate_data.py

--- data/generate_data.py
+++ data/generate_data.py
@@ -4,51 +4,30 @@ import json
 
 NUM_ROWS = 1000
 
-# task attributes
-DESCRIPTION = "Sample Description"
-COMPLETE_STATUS = False
-PERSON_ASSIGNED = "Sample Person Assigned"
-DUE_DATE = "Sample Due Date"
-ESTIMATED_DURATION = [1, 2, 3, 5, 8, 13]
+def generate_data():
+    projects = []
 
-# project attributes
-TEAM_
-BUDGETS = [10, 100, 1000, 10000]
-WORKLOADS = [1, 2, 3, 5, 8, 13]
+    for i in range(NUM_ROWS):
+        project = {
+            "projId": i,
+            "teamSize": random.choice([5, 10, 15, 20]),
+            "budget": random.choice([100, 1000, 10000]),
+            "workload": 0,
+            "completionTime": False,
+            "tasks": []
+        }
+        
+        for j in range(random.choice([1, 2, 3, 4])):
+            task = {
+                "taskId": j,
+                "description": random.choice(["Create", "Read", "Update", "Delete"]),
+                "personAssigned": random.choice(["Abby", "Bill", "Carolyn", "Darrel"]),
+                "dueDate": datetime.now().isoformat(),
+                "estimatedDuration": random.choice([1, 2, 3, 5, 8, 13])
+            }
+            project["workload"] += task["estimatedDuration"]
+            project["tasks"] += task
+        
+        projects.append(project)
 
-
-
-
-# def generate_data():
-#     projects = []
-
-#     for i in range(NUM_ROWS):
-
-
-#     return projects
-    # # Define the project template
-    # project_template = {
-    #     "proj_id": {
-    #         "size": "Large",
-    #         "color": "Yellow",
-    #         "pattern": "Plain",
-    #         "material": "Bamboo",
-    #         "condition": "Used",
-    #         "forFoot": "Both"
-    #     },
-    #     "additionalFeatures": {
-    #         "waterResistant": False,
-    #         "padded": False,
-    #         "antiBacterial": True
-    #     },
-    #     "addedTimestamp": datetime.now().isoformat()
-    # }
-
-    # # Create a list of three socks based on the template
-    # socks = [sock_template.copy() for _ in range(num_sock)]
-
-    # # Assign a unique timestamp to each sock
-    # for sock in socks:
-    #     sock['addedTimestamp'] = datetime.now().isoformat()
-
-    # return socks
+    return projects
