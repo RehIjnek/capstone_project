fe2a3bf41c8dc740d9294ec73cc4253664d37303
Kenji Her <kenjito7@gmail.com>
2024-07-15 12:57:55 -0400

    trained the model

--- data/generate_data.py
+++ data/generate_data.py
@@ -4,7 +4,7 @@ import json
 
 NUM_ROWS = 1000
 TEAM_SIZES = [5, 10, 15, 20]
-BUDGET_SIZES = [100, 1000, 10000]
+BUDGET_SIZES = [10, 100, 1000, 10000]
 WORKLOADS = ["light", "moderate", "heavy"]
 PROJECT_DURATIONS = [1, 2, 3, 5, 8, 13]
 
@@ -14,9 +14,9 @@ def generate_data():
     for i in range(NUM_ROWS):
         project = {
             "projId": i,
-            "teamSize": random.choice(TEAM_SIZES),
-            "budget": random.choice(BUDGET_SIZES),
-            "workload": random.choice(WORKLOADS),
+            "teamSize": 0,
+            "budget": 0,
+            "workload": "",
             "completionTime": 0,
             "tasks": []
         }
@@ -35,6 +35,18 @@ def generate_data():
             }
             project["completionTime"] += task["estimatedDuration"]
             project["tasks"].append(task)
+        if project["completionTime"] <= 10:
+            project["teamSize"] = random.choice([TEAM_SIZES[0], TEAM_SIZES[1]])
+            project["budget"] = random.choice([BUDGET_SIZES[0], BUDGET_SIZES[1]])
+            project["workload"] = WORKLOADS[0]
+        elif project["completionTime"] > 10 and project["completionTime"] < 30:
+            project["teamSize"] = random.choice([TEAM_SIZES[1], TEAM_SIZES[2]])
+            project["budget"] = random.choice([BUDGET_SIZES[1], BUDGET_SIZES[2]])
+            project["workload"] = WORKLOADS[1]
+        else:
+            project["budget"] = random.choice([BUDGET_SIZES[2], BUDGET_SIZES[3]])
+            project["teamSize"] = random.choice([TEAM_SIZES[2], TEAM_SIZES[3]])
+            project["workload"] = WORKLOADS[2]
         projects.append(project)
 
     return projects
